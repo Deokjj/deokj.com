c4bb3c60913394fa6cbaf966e2f935f0585bc64c
Deokjae <deokjj@gmail.com>
2017-12-02 16:39:59 -0500

    mobile improvement

--- src/components/Works.js
+++ src/components/Works.js
@@ -8,24 +8,62 @@ import YoutubeIcon from 'react-icons/lib/fa/youtube-play';
 import DesktopIcon from 'react-icons/lib/md/desktop-windows';
 
 import deokjdotcom from '../assets/deokjdotcom.png';
-import madClock from '../assets/madClock.png';
+import madClock from '../assets/madClock.gif';
 import tingrindr from '../assets/tingrindr.png';
 import copWatch from '../assets/copWatch.png';
 import toiletnotatrashcan from '../assets/toiletnotatrashcan.png';
+
+class TiltOrDiv extends Component {
+  constructor(props) {
+    super(props);
+    const isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
+    const isMobile = (typeof window.orientation !== 'undefined') ||  navigator.userAgent.match(/Android/i)
+                                                                 || navigator.userAgent.match(/webOS/i)
+                                                                 || navigator.userAgent.match(/iPhone/i)
+                                                                 || navigator.userAgent.match(/iPad/i)
+                                                                 || navigator.userAgent.match(/iPod/i)
+                                                                 || navigator.userAgent.match(/BlackBerry/i)
+                                                                 || navigator.userAgent.match(/Windows Phone/i);
+
+    const tiltOption = (isMobile) ? undefined : {
+       max:            (isChrome) ? 30 : 0,     // max tilt rotation (degrees)
+       perspective:    (isChrome) ? 1000 : 0,   // Transform perspective, the lower the more extreme the tilt gets.
+       scale:          1.05,      // 2 = 200%, 1.5 = 150%, etc..
+       speed:          1000,    // Speed of the enter/exit transition
+       transition:     false,   // Set a transition on enter/exit.
+     }
+    this.state={
+      isMobile: isMobile,
+      tiltOption: tiltOption
+    }
+  }
+
+  render() {
+    if(this.state.isMobile)
+      return (
+        <div className="project">
+          {this.props.children}
+        </div>
+      );
+    else
+      return(
+        <Tilt className="project" options={this.state.tiltOption}>
+          {this.props.children}
+        </Tilt>
+      );
+  }
+
+}
+
 
 
 class Works extends Component {
   constructor(props){
     super(props);
-    const isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
     this.state={
       clicked : false,
       selected : '',
-      isChrome: isChrome
     }
-  }
-
-  componentDidMount(){
   }
 
   projectSelect(project){
@@ -43,19 +81,11 @@ class Works extends Component {
   }
 
   render(){
-    const tiltOption={
-      max:            (this.state.isChrome) ? 30 : 0,     // max tilt rotation (degrees)
-      perspective:    (this.state.isChrome) ? 1000 : 0,   // Transform perspective, the lower the more extreme the tilt gets.
-      scale:          1.05,      // 2 = 200%, 1.5 = 150%, etc..
-      speed:          1000,    // Speed of the enter/exit transition
-      transition:     false,   // Set a transition on enter/exit.
-    }
-
     let snackBarMsg = (<h4>This Project Description has not been updated yet.</h4>);
     let openedImg;
 
     if(this.state.selected === 'deokjae'){
-      openedImg = (<img className="projectImg openImg" src = {deokjdotcom} alt=""/>);
+      openedImg = (<img className="projectImg openImg" src = {deokjdotcom} alt="project img appears here/may be loading"/>);
       snackBarMsg = (
         <div>
           <h4 className="highlight">Deokj.com</h4>
@@ -72,7 +102,7 @@ class Works extends Component {
       )
     }
     else if(this.state.selected === 'madClock'){
-      openedImg = (<img className="projectImg openImg" src = {madClock} alt=""/>);
+      openedImg = (<img className="projectImg openImg" src = {madClock} alt="project img appears here/may be loading"/>);
       snackBarMsg = (
         <div>
           <h4 className="highlight">Mad Alarm</h4>
@@ -133,7 +163,7 @@ class Works extends Component {
       );
     }
     else if(this.state.selected === 'tingrindr'){
-      openedImg = (<img className="projectImg openImg" src = {tingrindr} alt=""/>);
+      openedImg = (<img className="projectImg openImg" src = {tingrindr} alt="project img appears here/may be loading"/>);
       snackBarMsg = (
         <div>
           <h4 className="highlight">TinGrindr</h4>
@@ -160,7 +190,7 @@ class Works extends Component {
       );
     }
     else if(this.state.selected === 'copwatch'){
-      openedImg = (<img className="projectImg openImg" src = {copWatch} alt=""/>);
+      openedImg = (<img className="projectImg openImg" src = {copWatch} alt="project img appears here/may be loading"/>);
       snackBarMsg = (
         <div>
           <h4 className="highlight">CopWatch</h4>
@@ -170,7 +200,7 @@ class Works extends Component {
       );
     }
     else if(this.state.selected === 'toilet'){
-      openedImg = (<img className="projectImg openImg" src = {toiletnotatrashcan} alt=""/>);
+      openedImg = (<img className="projectImg openImg" src = {toiletnotatrashcan} alt="project img appears here/may be loading"/>);
       snackBarMsg = (
         <div>
           <h4 className="highlight">Destroy the Toilet</h4>
@@ -219,31 +249,31 @@ class Works extends Component {
           </h2>
         </VelocityTransitionGroup>
         <div className="flex projectList">
-          <Tilt className="project" options={tiltOption}>
+          <TiltOrDiv>
             <VelocityTransitionGroup enter={{animation: 'transition.perspectiveUpIn', delay:300}} runOnMount={true}>
               <img className="projectImg" src = {deokjdotcom} alt="" onClick={(e)=>{this.projectSelect('deokjae');}}/>
             </VelocityTransitionGroup>
-          </Tilt>
-          <Tilt className="project" options={tiltOption}>
+          </TiltOrDiv>
+          <TiltOrDiv>
             <VelocityTransitionGroup enter={{animation: 'transition.perspectiveUpIn', delay:500}} runOnMount={true}>
               <img className="projectImg" src = {madClock} alt="" onClick={(e)=>{this.projectSelect('madClock');}}/>
             </VelocityTransitionGroup>
-          </Tilt>
-          <Tilt className="project" options={tiltOption}>
+          </TiltOrDiv>
+          <TiltOrDiv>
             <VelocityTransitionGroup enter={{animation: 'transition.perspectiveUpIn', delay:700}} runOnMount={true}>
               <img className="projectImg" src = {tingrindr} alt="" onClick={(e)=>{this.projectSelect('tingrindr');}}/>
             </VelocityTransitionGroup>
-          </Tilt>
-          <Tilt className="project" options={tiltOption}>
+          </TiltOrDiv>
+          <TiltOrDiv>
             <VelocityTransitionGroup enter={{animation: 'transition.perspectiveUpIn', delay:900}} runOnMount={true}>
               <img className="projectImg" src = {copWatch} alt="" onClick={(e)=>{this.projectSelect('copwatch');}}/>
             </VelocityTransitionGroup>
-          </Tilt>
-          <Tilt className="project" options={tiltOption}>
+          </TiltOrDiv>
+          <TiltOrDiv>
             <VelocityTransitionGroup enter={{animation: 'transition.perspectiveUpIn', delay: 1100}} runOnMount={true}>
               <img className="projectImg" src = {toiletnotatrashcan} alt="" onClick={(e)=>{this.projectSelect('toilet');}}/>
             </VelocityTransitionGroup>
-          </Tilt>
+          </TiltOrDiv>
         </div>
         <VelocityTransitionGroup enter={{animation: 'transition.bounceDownIn', duration: 500}} leave={{animation: 'transition.bounceUpOut', duration: 500}}>
           {this.state.clicked ? <div className="overlay" style={{position: 'fixed',zIndex: 11}} onClick={(e)=>{this.projectClose();}}></div> : undefined}
