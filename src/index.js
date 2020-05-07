7f1dbe10aafecc86250b1ab769ac17d8e2f1c731
Deokjae <deokjj@gmail.com>
2020-05-07 19:00:18 -0400

    splitting versions

--- src/index.js
+++ src/index.js
@@ -1,7 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import {useRoutes} from 'hookrouter';
 import App from './App';
-import OldApp from './version0/App';
+// import OldApp from './version0/App';
+const OldApp = () => {
+    const Component = React.lazy(() => import('./version0/App'));
+    return (
+        <React.Suspense fallback={<div>Loading...</div>}>
+            <Component/>
+        </React.Suspense>
+    )
+};
 
-ReactDOM.render(<OldApp/>, document.getElementById('root'));
+const routes = {
+    '/': () => <App />,
+    '/v0': () => <OldApp />
+};
+
+const Router = () => {
+    const Routes = useRoutes(routes);
+
+    return Routes || <App />
+}
+
+ReactDOM.render(<Router/>, document.getElementById('root'));
